56f258c4522fe90927bc4949804c4dd1351f8346
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-10 23:16:31 +0300

    1

--- fundamentals/Functions - Lab/signCheck.js
+++ fundamentals/Functions - Lab/signCheck.js
@@ -1,6 +1,20 @@
-function signCheck(numOne,numTwo,numTree) {
-    let result = 0;
+function signCheck(numOne, numTwo, numTree) {
 
-    
+    let negative = 0;
+    let num = [];
+    num.push(numOne, numTwo, numTree)
+  
+    for (let i = 0; i < num.length; i++) {
+let n = num[i]
+        if (n < 0) {
+            negative++;
+        }
+    }
+    if (negative % 2 === 0) {
+        console.log("Positive");
+    } else {
+        console.log("Negative");
+    }
+
 }
-signCheck ( 5,12,-15)
+signCheck(-6, -12, 14)
