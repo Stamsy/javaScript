a4f1943fcbd51c6e104917fec1598c38983d8ef1
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-11 09:45:08 +0300

    1

---
+++ fundamentals/Functions - Exercise/addAndSubtract.js
@@ -0,0 +1,13 @@
+function addAndSubtract(one, two, tree) {
+    let firstSum = sum(one, two);
+    let finalResult = substract(firstSum, tree)
+
+    console.log(finalResult);
+    function sum(first, second) {
+        return one + two
+    }
+    function substract(first, second) {
+        return firstSum - tree
+    }
+}
+addAndSubtract(23, 6, 10)
