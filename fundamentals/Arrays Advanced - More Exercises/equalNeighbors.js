65dd64dee6395837be7a73593852e925d292040f
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-21 21:12:17 +0300

    1

---
+++ fundamentals/Arrays Advanced - More Exercises/equalNeighbors.js
@@ -0,0 +1,31 @@
+function equalNeighbors(matrix) {
+    let equalPairs = 0;
+
+    for (let row = 0; row < matrix.length; row++) {
+        let currentRow = matrix[row];
+        let nextRow;
+        if (row + 1 < matrix.length) {
+            nextRow = matrix[row + 1];
+        } else {
+            nextRow = [];
+        }
+        for (let j = 0; j < currentRow.length; j++) {
+            if (currentRow[j] === currentRow[j + 1] || currentRow[j] === nextRow[j]) {
+                if (
+                    currentRow[j] === currentRow[j + 1] &&
+                    currentRow[j] === nextRow[j]
+                ) {
+                    equalPairs += 2;
+                } else {
+                    equalPairs++;
+                }
+            }
+        }
+    }
+
+    console.log(equalPairs);
+}
+equalNeighbors([['2', '3', '4', '7', '0'],
+['4', '0', '5', '3', '4'],
+['2', '3', '5', '4', '2'],
+['9', '8', '7', '5', '4']])
