17746f1d61f2f1dc6e6a1a4378abf1b4db398a47
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-22 16:28:13 +0300

    1

--- fundamentals/exams/3/movingTarget.js
+++ fundamentals/exams/3/movingTarget.js
@@ -1,27 +1,56 @@
-function movingTarget(input) {
-    let targets = input.shift().split(' ').map(x => Number(x));
-    let token = input.shift().split(" ");
+function movingTarget(arr) {
 
-    for(let i =0; i< token.length; i++){
-        let command = token[i];
-        let indexNum = token[i +1];
-        let 
+    let targets = arr.shift().split(' ').map(Number);
+    let index = 0;
+    let command = arr[index];
+    index++;
 
-        switch(command){
-            case "Shoot ":
+    while (command !== 'End') {
+        command = command.split(' ');
+        let token = command[0]
+        let indexOne = Number(command[1]);
+        let indexTwo = Number(command[2]);
+        switch (token) {
+            case 'Shoot':
+                if (indexOne < targets.length) {
+                    targets[indexOne] -= indexTwo
+                }
+                if (targets[indexOne] <= 0) {
+                    targets.splice(indexOne, 1);
+                }
                 break;
-                case"Add":
+
+            case 'Add':
+                if (indexOne >= 0 && indexOne < targets.length) {
+                    targets.splice(indexOne, 0, indexTwo)
+                } else {
+                    console.log("Invalid placement!");
+                }
                 break;
-                case"Strike":
+
+            case 'Strike':
+                let radiusLeft = indexOne - indexTwo;
+                let radiusRight = indexTwo * 2 + 1
+                if (indexOne + indexTwo <= targets.length - 1 && indexOne - indexTwo >= 0) {
+                    targets.splice(radiusLeft, radiusRight);
+                } else {
+                    console.log("Strike missed!");
+                }
                 break;
         }
+        command = arr[index];
+        index++;
+
     }
-    console.log(token);
-    
+    console.log(targets.join('|'));
 }
+movingTarget
+    (["1 2 3 4 5",
+        "Strike 0 1",
+        "End"]);
 movingTarget(["52 74 23 44 96 110",
-"Shoot 5 10",
-"Shoot 1 80",
-"Strike 2 1",
-"Add 22 3",
-"End"])
+    "Shoot 5 10",
+    "Shoot 1 80",
+    "Strike 2 1",
+    "Add 22 3",
+    "End"]);
