7b14427673037ccc9f00f17cd6a744e91fd7b667
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-22 16:28:21 +0300

    1

--- fundamentals/proba.js
+++ fundamentals/proba.js
@@ -1,28 +1,72 @@
-function equalNeighbors(matrix) {
-    let equalPairs = 0;
+function shoppingList(arr) {
  
-    for (let row = 0; row < matrix.length; row++){
-        
-        let currentRow = matrix[row];
-        let nextRow;
-        if (row + 1 < matrix.length) {
-            nextRow = matrix[row + 1]
-        } else {
-            nextRow = [];
+    let arrOfProducts = arr.shift().split('!');
+    let index = 0;
+    let command = arr[index];
+    index++;
+ 
+    while (command !== 'Go Shopping!') {
+        command = arr[index].split(' ');
+        let token = command[0];
+ 
+        switch (token) {
+            case 'Urgent':
+                let productOne = command[1];
+                if (arrOfProducts.includes(productOne)) {
+                    command = arr[index];
+                    index++;
+                    continue;
+                } else {
+                    arrOfProducts.unshift(productOne);
+                }
+                break;
+            case 'Unnecessary':
+                let productTwo = command[1];
+                if (!arrOfProducts.includes(productTwo)) {
+                    command = arr[index];
+                    index++;
+                    continue;
+                } else {
+                    let indexOfProductTwo = arrOfProducts.indexOf(productTwo);
+                    arrOfProducts.splice(indexOfProductTwo, 1);
+                }
+                break;
+            case 'Correct':
+                let oldItem = command[1];
+                let newItem = command[2];
+                if (!arrOfProducts.includes(oldItem)) {
+                    command = arr[index];
+                    index++;
+                    continue;
+                } else {
+                    let indexOfOldItem = arrOfProducts.indexOf(oldItem);
+                    arrOfProducts.splice(indexOfOldItem, 1, newItem);
+                }
+                break;
+            case 'Rearrange':
+                let itemOfRearrange = command[1];
+                if (!arrOfProducts.includes(itemOfRearrange)) {
+                    command = arr[index];
+                    index++;
+                    continue;
+                } else {
+                    let indexOfRearrange = arrOfProducts.indexOf(itemOfRearrange);
+                    arrOfProducts.splice(indexOfRearrange, 1);
+                    arrOfProducts.push(itemOfRearrange);
+                }
+                break;
         }
-        for (let j = 0; j < currentRow.length; j++){
-            if (currentRow[j] == currentRow[j + 1] && currentRow[j]== nextRow [j]) {
-                equalPairs++;
-            }
-        }
+        command = arr[index];
+        index++;
     }
- 
- 
-console.log(equalPairs);
+    arrOfProducts = arrOfProducts.join(', ');
+    console.log(arrOfProducts);
  
 }
-equalNeighbors([['test', 'yo', 'yo', 'ho'],
-['well', 'done', 'no', '6'],
-['not', 'done', 'yet', '5']]
-
-)
+shoppingList(["Milk!Pepper!Salt!Water!Banana",
+"Urgent Salt",
+"Unnecessary Grapes",
+"Correct Pepper Onion",
+"Rearrange Grapes",
+"Correct Tomatoes Potatoes",
+"Go Shopping!"]);
