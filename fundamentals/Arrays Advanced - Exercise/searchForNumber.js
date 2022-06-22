3880366c21a70c89e2642567d47f2018acf0336f
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-22 10:05:15 +0300

    1

---
+++ fundamentals/Arrays Advanced - Exercise/searchForNumber.js
@@ -0,0 +1,21 @@
+function searchForNumber(numsOne,numsTwo) {
+ 
+    let takenEl = numsTwo[0];
+    let deleteEl = numsTwo[1];
+    let searchEl = numsTwo[2];
+    let searchCount =0;
+ 
+    let result =  numsOne.slice(0,takenEl);
+    result.splice(0,deleteEl)
+ 
+    for(let i = 0; i < result.length;i++){
+        if(result.includes(searchEl)){
+            searchCount++
+            let del = result.indexOf(searchEl)
+            result.splice(del,1)
+            i--
+        }
+    }
+    console.log(`Number ${searchEl} occurs ${searchCount} times.`);
+ 
+}
