879a92ee1a60391fa41ea0af70da3d024697b2f7
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-01 00:05:57 +0300

    1

--- fundamentals/Objects and Classes - Lab/city.js
+++ fundamentals/Objects and Classes - Lab/city.js
@@ -1,10 +1,15 @@
-function city(params) {
-    
+function cityInfo(city) {
+let props = Object.keys(city);
+
+for(let prop of props){
+    console.log((`${prop} -> ${city[prop]}`));
 }
-city({
-    name: "Sofia",
-    area: 492,
-    population: 1238438,
-    country: "Bulgaria",
-    postCode: "1000"
-})
+}
+
+        cityInfo({
+            name: "Plovdiv",
+            area: 389,
+            population: 1162358,
+            country: "Bulgaria",
+            postCode: "4000"
+        });
