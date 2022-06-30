ac98a4c4cddfa217937de70afba8e451cf394f82
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-06-30 23:05:49 +0300

    1

--- fundamentals/exams/reatake exam/softUniKaraoke.js
+++ fundamentals/exams/reatake exam/softUniKaraoke.js
@@ -1,14 +1,45 @@
 function softUniKaraoke(arr) {
-    let singers = arr.shift( ).split(", ")
-    let songs = arr.shift().split(" - ")
-    
-    while (arr[0] !== "dawn") {
-        let command = arr.shift().split(", ")
-        let participant = command.shift();
-        let song = command[0];
-        let award = command[1]
+    let participants = arr.shift().split(", ");
+    let songs = arr.shift().split(", ");
+    let index = 0;
+    let performance = arr[index];
+    index++;
+    let resName = "";
+    let resAward = "";
+    let counter = 0;
+    let isIncludes = false;
+ 
+    while (performance !== "dawn") {
+        performance = performance.split(", ");
+        let name = performance[0];
+        let songPerformance = performance[1];
+        let award = performance[2];
+ 
+        if (participants.includes(name) && songs.includes(songPerformance)) {
+            isIncludes = true;
+            counter++;
+            let currentName = name;
+            if (resName !== currentName) {
+                resName = currentName;
+                console.log(`${resName}: ${counter} awards`);
+                counter = 0;
+            }
+            let currentAward = "";
+            currentAward = award;
+            if (currentAward !== resAward) {
+                resAward = currentAward;
+                console.log(`--${resAward}`);
+            }
+ 
+        }
+        performance = arr[index];
+        index++;
     }
-
+    if (performance === "dawn") {
+        if (isIncludes === false) {
+            console.log("No awards");
+        }
+    }
 }
 softUniKaraoke(["Trifon, Vankata, Gesha",
     "Dragana - Kukavice, Bon Jovi - It's my life,Lorde - Royals",
