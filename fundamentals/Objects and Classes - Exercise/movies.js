62b03b0414e6ccea6747861e9440b6bb0f0b2c61
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-04 09:07:00 +0300

    1

--- fundamentals/Objects and Classes - Exercise/movies.js
+++ fundamentals/Objects and Classes - Exercise/movies.js
@@ -1,11 +1,37 @@
 function movies(input) {
-    
+
     let movies = [];
 
-    for(let line of input){
+    for (let line of input) {
 
+        if (line.includes('addMovie')) {
+            let name = line.split('addMovie ')[1]
+            movies.push({ name })
+        } else if (line.includes('directedBy')) {
+            let [name, director] = line.split(' directedBy ')
+
+            let movie = movies.find((movieObj => movieObj.name === name))
+
+            if (movie) {
+                movie.director = director;
+            }
+        } else if (line.includes('onDate')) {
+            let [name, date] = line.split(' onDate ')
+
+            let movie = movies.find((movieObj => movieObj.name === name))
+
+            if (movie) {
+                movie.date = date;
+            }
+        }
     }
+    movies.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    });
 }
+
 movies([
     'addMovie Fast and Furious',
     'addMovie Godfather',
@@ -15,4 +41,4 @@ movies([
     'Fast and Furious onDate 30.07.2018',
     'Batman onDate 01.08.2018',
     'Fast and Furious directedBy Rob Cohen'
-    ])
+])
