c8ae700dd1621ce808ff3ed275bc75cf12efd8c9
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-12 23:30:43 +0300

    1

---
+++ fundamentals/Objects and Classes - More Exercises/flightSchedule.js
@@ -0,0 +1,56 @@
+function flightSchedule(arr) {
+    class Flight{
+        constructor(flyNumber,destination){
+            this.flyNumber = flyNumber;
+            this.destination = destination;
+            this.status = "Ready to fly";
+        }
+    }
+    let result = [];
+    
+    for(let flightInfo of arr[0]){
+        let token = flightInfo.split(" ");
+        let flightNumber = token.shift();
+        let destination = token.join(' ');
+       
+        let myFlight = new Flight(flightNumber, destination);
+        result.push(myFlight)
+    }
+    for(let token of arr[1]){
+        let tokens = token.split(' ')
+       let findFlight =  result.filter(obj => obj.flyNumber === tokens[0]);
+
+       if(findFlight){
+        findFlight.status = 'Cancelled'
+       }
+    }
+    let command = arr[2][0];
+    switch(command){
+        case "Cancelled":
+            let canceldFly = result.filter(obj => obj.status === "Cancelled");
+            canceldFly.forEach(x => console.log(`{ Destination: '${x.destination}', Status: '${x.status}'`))
+            break;
+         case "Ready to fly":
+            let readyToFly = result.filter(obj => obj.status === "Ready to fly")
+            readyToFly.forEach(x => console.log(`{ Destination: '${x.destination}', Status: '${x.status}'`))
+            break;
+    
+    }
+}
+flightSchedule([[
+ 'WN269 Delaware',
+ 'FL2269 Oregon',
+ 'WN498 Las Vegas',
+ 'WN3145 Ohio',
+ 'WN612 Alabama',
+ 'WN4010 New York',
+ 'WN1173 California',
+ 'DL2120 Texas',
+ 'KL5744 Illinois',
+ 'WN678 Pennsylvania'],
+ ['DL2120 Cancelled',
+ 'WN612 Cancelled',
+ 'WN1173 Cancelled',
+ 'SK430 Cancelled'],
+ ['Cancelled']
+])
