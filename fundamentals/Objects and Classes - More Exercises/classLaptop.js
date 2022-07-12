5937864b0105189965b7803d00948ef7b10c9eee
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-12 14:34:09 +0300

    1

---
+++ fundamentals/Objects and Classes - More Exercises/classLaptop.js
@@ -0,0 +1,32 @@
+ class Laptop{
+    constructor( info,quality){
+        this.info = info;
+        this.quality = quality;
+        this.isOn = false;
+        this.price = 800 - (this.info.age * 2) + (this.quality * 0.5);
+    }
+    turnOn(){
+        this.isOn = true;
+        this.quality --;
+        this.price -= 0.5;
+    }
+    turnOff(){
+        this.isOn = false;
+        this.quality --;
+        this.price -= 0.5;
+    }
+    showInfo(){
+        return JSON.stringify(this.info);
+    }
+    
+ }
+    
+ let info = {producer: "Dell", age: 2, brand: "XPS"}
+let laptop = new Laptop(info, 10)
+laptop.turnOn()
+console.log(laptop.showInfo())
+laptop.turnOff()
+console.log(laptop.quality)
+laptop.turnOn()
+console.log(laptop.isOn)
+console.log(laptop.price)
