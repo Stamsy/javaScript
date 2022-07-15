d2666a8b0cdc33fea4b95ce572b182b277e4532f
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-16 00:10:12 +0300

    1

--- fundamentals/Text Processing - Exercise/extractFile.js
+++ fundamentals/Text Processing - Exercise/extractFile.js
@@ -1,4 +1,9 @@
 function extractFile(input) {
-    let fileName = 
+    let index =  input.lastIndexOf("\\");
+    let fileName = input.substring(index + 1).split(".");
+    let extension = fileName.pop();
+    let template = fileName.join(".")
+    console.log(`File name: ${template}`);
+    console.log(`File extension: ${extension}`)
 }
 extractFile('C:\\Internal\\training-internal\\Template.pptx')
