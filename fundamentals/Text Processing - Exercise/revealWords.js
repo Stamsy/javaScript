79f1c57cdba8dd15668bb0cbc225678e7cb0f0f6
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-15 23:06:48 +0300

    1

--- fundamentals/Text Processing - Exercise/revealWords.js
+++ fundamentals/Text Processing - Exercise/revealWords.js
@@ -1,4 +1,12 @@
-function revealWords(params) {
+function revealWords(words, sentence) {
+    let wordsArr = words.split(", ");
     
+    for(let word of wordsArr){
+        let template = "*".repeat(word.length);
+        sentence = sentence.replace(template,word)
+     
+    }
+    console.log(sentence);
 }
-revealWords()
+revealWords('great',
+'softuni is ***** place for learning new programming languages')
