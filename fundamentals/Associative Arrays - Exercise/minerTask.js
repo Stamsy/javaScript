172042c2f3e16b5d6743ad843e0efa78c3181e91
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-20 15:37:04 +0300

    1

---
+++ fundamentals/Associative Arrays - Exercise/minerTask.js
@@ -0,0 +1,17 @@
+function minerTask(arr){
+let result = {}
+for(let i = 0; i < arr.length; i++){
+   if(i %2 !==0){
+    resut
+   }
+}
+console.log(result);
+}
+minerTask([
+    'Gold',
+    '155',
+    'Silver',
+    '10',
+    'Copper',
+    '17'
+    ])
