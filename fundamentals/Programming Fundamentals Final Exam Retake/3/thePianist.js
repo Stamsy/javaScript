74a611407998a848ceff96b8ae6e85a20e4fd3ed
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-26 14:06:45 +0300

    1

---
+++ fundamentals/Programming Fundamentals Final Exam Retake/3/thePianist.js
@@ -0,0 +1,16 @@
+function thePianist(input) {
+    let num = Number(input.shift());
+    console.log(num);
+}
+thePianist([
+    '3',
+    'Fur Elise|Beethoven|A Minor',
+    'Moonlight Sonata|Beethoven|C# Minor',
+    'Clair de Lune|Debussy|C# Minor',
+    'Add|Sonata No.2|Chopin|B Minor',
+    'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
+    'Add|Fur Elise|Beethoven|C# Minor',
+    'Remove|Clair de Lune',
+    'ChangeKey|Moonlight Sonata|C# Major',
+    'Stop'  
+  ])
