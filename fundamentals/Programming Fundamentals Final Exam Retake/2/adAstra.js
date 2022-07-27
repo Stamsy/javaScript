ac78dc86130f67927e234fefb1872a38036cd3ef
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-07-27 13:28:49 +0300

    1

--- fundamentals/Programming Fundamentals Final Exam Retake/2/adAstra.js
+++ fundamentals/Programming Fundamentals Final Exam Retake/2/adAstra.js
@@ -7,22 +7,22 @@ function adAstra(input) {
 
     let textInfo = [];
     let total = 0;
-    
+
 
     while (match !== null) {
         let { product, date, calories } = match.groups;
         calories = Number(calories);
         total += calories;
-        textInfo.push({product, date, calories});
+        textInfo.push({ product, date, calories });
         match = patternInput.exec(text)
     }
-        let needDay = Math.floor(total / calories)
-        console.log(`You have food to last you for: ${needDay} days!`);
+    let needDay = Math.floor(total / calories)
+    console.log(`You have food to last you for: ${needDay} days!`);
 
-        textInfo.forEach(obj => {
-            console.log(`Item: ${obj.product}, Best before: ${obj.date}, Nutrition: ${obj.calories}`);
-          });
-    }
+    textInfo.forEach(obj => {
+        console.log(`Item: ${obj.product}, Best before: ${obj.date}, Nutrition: ${obj.calories}`);
+    });
+}
 adAstra([
     '$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|'
 ])
--- fundamentals/Programming Fundamentals Final Exam Retake/3/thePianist.js
+++ fundamentals/Programming Fundamentals Final Exam Retake/3/thePianist.js
@@ -1,6 +1,65 @@
 function thePianist(input) {
     let num = Number(input.shift());
-    console.log(num);
+    let pianoPieces = {};
+
+    for (let i = 0; i < num; i++) {
+        let pianoList = input.shift().split("|")
+        let piece = pianoList[0];
+        let composer = pianoList[1];
+        let key = pianoList[2];
+        pianoPieces[piece] = {
+            composer,
+            key
+        };
+    }
+    while (input[0] != "Stop") {
+        let tokens = input.shift().split("|");
+        let command = tokens[0];
+        let piece = tokens[1];
+        let composer = tokens[2];
+        let key = tokens[3];
+
+        switch (command) {
+            case "Add":
+                if (pianoPieces.hasOwnProperty(piece)) {
+                    console.log(`${piece} is already in the collection!`);
+                    break;
+                } else {
+                    pianoPieces[piece] = {
+                        composer,
+                        key
+                    };
+                   
+                    console.log(`${piece} by ${composer} in ${key} added to the collection!`);
+                }
+                break;
+
+            case "Remove":
+                if (pianoPieces.hasOwnProperty(piece)) {
+                    delete pianoPieces[piece];
+                    console.log(`Successfully removed ${piece}!`);
+                } else {
+                    console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+                    break;
+                }
+                break;
+            case "ChangeKey":
+                if (pianoPieces.hasOwnProperty(piece)) {
+                    pianoPieces[piece].key = composer;
+                    console.log(`Changed the key of ${piece} to ${composer}!`);
+                } else {
+                    console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+                }
+                break;
+        }
+    }
+    for (let el of Object.entries(pianoPieces)) {
+        let name = el[0];
+        let nameComp = pianoPieces[el[0]].composer;
+        let keyName = pianoPieces[el[0]].key;
+        console.log(`${name} -> Composer: ${nameComp}, Key: ${keyName}`);
+    }
+
 }
 thePianist([
     '3',
@@ -12,5 +71,5 @@ thePianist([
     'Add|Fur Elise|Beethoven|C# Minor',
     'Remove|Clair de Lune',
     'ChangeKey|Moonlight Sonata|C# Major',
-    'Stop'  
-  ])
+    'Stop'
+])
