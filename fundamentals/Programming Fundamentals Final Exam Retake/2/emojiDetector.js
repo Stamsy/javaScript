6282d49a3aaf7dc5ff504254a3de26b4fa3a03d2
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-08-03 16:17:46 +0300

    1

--- fundamentals/Programming Fundamentals Final Exam Retake/2/emojiDetector.js
+++ fundamentals/Programming Fundamentals Final Exam Retake/2/emojiDetector.js
@@ -1,4 +1,28 @@
 function emojiDetector(input) {
-    
+    let pattern = /(::|\*\*)(?<emoji>[A-Z][a-z]{2,})\1/g
+    let match;
+    let coolNum = 1
+    let machNum = /\d/g
+    let cool = input[0].match(machNum)
+
+    for (let i = 0; i < cool.length; i++) {
+        coolNum *= cool[i]
+    }
+    console.log(`Cool threshold: ${coolNum}`);
+    let foundEm = input[0].match(pattern).length
+    console.log(`${foundEm} emojis found in the text. The cool ones are:`)
+
+    while ((match = pattern.exec(input[0])) != null) {
+        let emoji = match.groups["emoji"];
+        let currentSum = 0
+        for (let letter of emoji) {
+            currentSum += letter.charCodeAt()
+        }
+        if (currentSum >= coolNum) {
+            console.log(match[0]);
+        }
+    }
+
 }
+
 emojiDetector(["In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**"])
