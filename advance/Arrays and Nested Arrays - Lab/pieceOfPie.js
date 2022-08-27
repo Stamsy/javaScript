2f915fca9163db7c8d94b255f354b338c0fd3757
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-08-28 00:08:08 +0300

    1

--- advance/Arrays and Nested Arrays - Lab/pieceOfPie.js
+++ advance/Arrays and Nested Arrays - Lab/pieceOfPie.js
@@ -1,4 +1,21 @@
-function solve(params) {
-    
+function solve(arrey, one,two) {
+    let start = 0;
+    let end = 0;
+    if(arrey.includes(one)){
+start = arrey.indexOf(one)
+    }
+    if(arrey.includes(two)){
+        end = arrey.indexOf(two)
+    }
+    let newArrey = arrey.slice(start,end + 1);
+    return newArrey;
 }
-solve()
+
+solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie')
