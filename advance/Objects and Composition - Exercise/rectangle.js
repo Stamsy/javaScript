4a36958bd2e3669e258bf8b80d848cdd1bab9871
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-09-27 00:39:07 +0300

    1

---
+++ advance/Objects and Composition - Exercise/rectangle.js
@@ -0,0 +1,21 @@
+function rectangle(width, height, color){
+class Rectangle{
+    constructor(width, height,color){
+        this.width = width;
+        this.height = height;
+        this.color = color[0].toUpperCase() + color.slice(1);
+    }
+    calcArea(){
+        return this.height*this.width;
+       //console.log(this.height * this.width);
+    }
+}
+
+return new Rectangle(width, height, color)
+//console.log(new Rectangle(width, height, color));
+}
+    let rect = rectangle(4, 5, 'red');
+    console.log(rect.width);
+    console.log(rect.height);
+    console.log(rect.color);
+    console.log(rect.calcArea());
