d2e6fb3ce77472d2ae69c0496dc8ed42d320be1d
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-09-27 18:35:00 +0300

    1

--- advance/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ advance/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
+    //const rows = document.querySelectorAll('tr');
+    //for(let i = 1; i < rows.length; i+=2){
+    //    rows[i].style.background = 'teal'
+   // }
+
+   const rows = document.querySelectorAll('tr:nth-child(2n)');
+
+   for(let i = 0; i < rows.length; i++){
+    rows[i].style.background = 'teal'
+   }
 }
