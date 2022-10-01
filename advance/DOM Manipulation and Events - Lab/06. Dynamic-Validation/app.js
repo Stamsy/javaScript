4b6c8f2aa10b31a44cde38ee0f40f0b66e07d608
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-10-02 00:52:45 +0300

    1

--- advance/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ advance/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,26 @@
 function validate() {
-    console.log('TODO:...');
-}
+    document.getElementById("email").addEventListener("change", validEmail); // 'change' <=> 'input'
+    document.getElementById("email").value;
+    const match = /^[a-z]+@[a-z]+\.[a-z]{2,}/gm;
+  //   let isValid = false;
+  
+  //   *** With .addEventListener </change> ***
+    
+      function validEmail(input) {
+        match.test(input.target.value)
+          ? input.target.removeAttribute("class")
+          : input.target.className = "error";
+      }
+  
+      // *** With .addEventListener </input> ***
+  
+  //   function validEmail(input) {
+  //     if (match.test(input.target.value)) {
+  //       isValid = true;    
+  //       input.target.removeAttribute("class");
+  //     } else {
+  //       isValid = false;    
+  //       input.target.className = "error";
+  //     }
+  //   }
+  }
--- advance/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ advance/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
-}
+   const textArea = document.querySelector("textarea");
+   const addButton = [...document.querySelectorAll("div[class=product-add]")];
+   addButton.forEach((button) => button.addEventListener("click", addProducts));
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkOut)
+   const shoppingCart = [];
+   textArea.value = ''
+ 
+   function addProducts(btn) {
+     if (btn.target = "BUTTON") {
+       const product = btn.target.parentElement.parentElement;
+       const productName = product.querySelector(".product-title").textContent;
+       const productPrice = Number(product.querySelector(".product-line-price").textContent);
+       shoppingCart.push({
+         productName,
+         productPrice,
+       });
+       textArea.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+    }
+   }
+   function checkOut() {
+    let finalProducts = new Set();
+    shoppingCart.forEach(product => finalProducts.add(product.productName));
+    let finalSum = shoppingCart.reduce((a, b) => a + b.productPrice, 0);
+    textArea.value += `You bought ${[...finalProducts.keys()].join(', ')} for ${finalSum.toFixed(2)}.`
+    document.getElementsByClassName('checkout')[0].disabled = true;
+    [...document.getElementsByTagName("button")].forEach(button => button.disabled = true);
+   }
+ }
