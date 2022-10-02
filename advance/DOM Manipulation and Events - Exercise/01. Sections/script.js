772014ba70ff25775201b5917abd08e53b819773
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-10-02 09:18:44 +0300

    1

--- advance/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ advance/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,19 @@
-function create(words) {
-   console.log('TODO:...');
+function create(input) {
+   let parentElement = document.getElementById('content');
+   let elements = input;
+
+   for(let i = 0; i < elements.length;i++){
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      let text = document.createTextNode(elements[i]);
+
+      p.appendChild(text);
+      p.style.display = 'none';
+      div.appendChild(p); 
+      div.addEventListener('click',showParagraphs);
+      parentElement.appendChild(div);
+   }
+   function showParagraphs(event){
+      event.target.children[0].style.display = '';
+   }
 }
