5b98790a4f9634ee662242095d51337722658e9b
Stanimira Rikova <104592468+Stams89@users.noreply.github.com>
2022-10-04 07:42:59 +0300

    1

---
+++ advance/Advanced Functions - Lab/areaVol.js
@@ -0,0 +1,14 @@
+function areaVol(areaIn,volIn,input){
+
+}
+
+areaVol(area,vol, );
+areaVol(area,vol, );
+
+function area(){
+return Math.abs(this.x * this.y);
+};
+
+function vol(){
+return Math.abs(this.x * this.y * this.z);
+};
