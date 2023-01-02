a7371691f248867e70a3d3098a2ab20260ab9815
Stanimira Rikova <stams_89@abv.bg>
2023-01-02 21:59:01 +0200

    sreams

--- JS back-end/node.js streams and utilities/observer.js
+++ JS back-end/node.js streams and utilities/observer.js
@@ -1,25 +1,46 @@
-const subscribers = {};
+// const subscribers = {};
 
-function subscribe(type, callback) {
-    console.log('new subscriber for ' + type);
-    if (subscribers[type] == undefined) {
-        subscribers[type] = [];
+// function subscribe(type, callback) {
+//     console.log('new subscriber for ' + type);
+//     if (subscribers[type] == undefined) {
+//         subscribers[type] = [];
+//     }
+//     subscribers[type].push(callback);
+// }
+
+// function publish(type, data) {
+//     console.log('received' + type);
+//     const currentSubscribers = subscribers[type];
+
+//     if (currentSubscribers) {
+//         for (let subscriber of currentSubscribers) {
+//             subscriber(data);
+//         }
+//     }
+// }
+
+// module.exports = {
+//     subscribe,
+//     publish
+// };
+
+const http = reqire('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method == 'GET') {
+        res.write('OK');
+        res.end();
+    } else if (req.method == 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            console.log(body.join(''));
+            res.end(); 
+        });
     }
-    subscribers[type].push(callback);
-}
 
-function publish(type, data) {
-    console.log('received' + type);
-    const currentSubscribers = subscribers[type];
+});
 
-    if (currentSubscribers) {
-        for (let subscriber of currentSubscribers) {
-            subscriber(data);
-        }
-    }
-}
-
-module.exports = {
-    subscribe,
-    publish
-};
+server.listen(3000);
